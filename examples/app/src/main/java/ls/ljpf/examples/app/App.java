fe5d3cadac4ce9f13d5eb77f941097545799937e
Luke Sosnicki <luke.sosnicki@me.com>
2017-03-30 21:38:17 +0200

    Explicit classloaderfactory in App Example

--- examples/app/src/main/java/ls/ljpf/examples/app/App.java
+++ examples/app/src/main/java/ls/ljpf/examples/app/App.java
@@ -1,9 +1,11 @@
 package ls.ljpf.examples.app;
 
+import ls.ljpf.PluginClassLoaderFactory;
 import ls.ljpf.PluginManager;
 import ls.ljpf.PluginRepository;
 import ls.ljpf.examples.common.ExampleConfig;
 import ls.ljpf.impl.DefaultPluginManager;
+import ls.ljpf.loader.ParentLastClassLoaderFactory;
 import ls.ljpf.repository.ClasspathPluginRepository;
 
 /**
@@ -15,11 +17,13 @@ public class App {
 
     public void init() {
         PluginRepository pluginRepository = new ClasspathPluginRepository(".*ls.ljpf.examples.*jar");
+        PluginClassLoaderFactory classLoaderFactory = new ParentLastClassLoaderFactory();
+
+        pluginManager = new DefaultPluginManager(pluginRepository, classLoaderFactory);
 
         ExampleConfig appConfig = new ExampleConfig();
         appConfig.setValue("Hello");
 
-        pluginManager = new DefaultPluginManager(pluginRepository);
         pluginManager.load("FirstPlugin", appConfig);
         pluginManager.load("SecondPlugin", appConfig);
 
